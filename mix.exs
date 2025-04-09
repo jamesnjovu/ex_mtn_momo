161d6bb184199d516ba534802f10ce14988c673e
James <njovujames@gmail.com>
2025-04-09 15:55:04 +0200

    Bump version to 0.1.2 in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExMtnMomo.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/jamesnjovu/ex_mtn_momo"
   @description "Elixir library for MTN Mobile Money API integration"
 
