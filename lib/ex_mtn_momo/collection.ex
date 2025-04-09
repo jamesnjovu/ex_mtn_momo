65813e74f490ad8b5a99167b7a7aa43de7393267
James <njovujames@gmail.com>
2025-04-09 08:22:53 +0200

    Add methods for transaction status, user info, and payment status retrieval in collection API

--- lib/ex_mtn_momo/collection.ex
+++ lib/ex_mtn_momo/collection.ex
@@ -54,4 +54,55 @@ defmodule ExMtnMomo.Collection do
     end
   end
 
+  def request_to_withdraw_transaction_status(reference_id, access_token,  options \\ []) do
+    base_url = Util.extract_base_url(options)
+
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{base_url}/collection/v1_0/requesttowithdraw/#{reference_id}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def basic_user_info(msisdn, access_token,  options \\ []) do
+    base_url = Util.extract_base_url(options)
+
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{base_url}/collection/v1_0/accountholder/msisdn/#{msisdn}/basicuserinfo"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_payment_status(x_reference_id, access_token,  options \\ []) do
+    base_url = Util.extract_base_url(options)
+
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{base_url}/collection/v2_0/payment/#{x_reference_id}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
 end
