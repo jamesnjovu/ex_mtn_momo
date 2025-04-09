015a74ab3f924197d08800f144c41a4ef07ab92b
James <njovujames@gmail.com>
2025-04-09 10:52:11 +0200

    Add new API methods for token creation, account balance retrieval, and transaction status checks

--- lib/ex_mtn_momo/collection.ex
+++ lib/ex_mtn_momo/collection.ex
@@ -2,15 +2,28 @@ defmodule ExMtnMomo.Collection do
   alias ExMtnMomo.Util
 
   def create_access_token(options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Content-Type", "application/json"},
       {"Authorization", Util.collection_auth(options)},
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)}
     ]
 
-    "#{base_url}/collection/token/"
+    "#{Util.extract_base_url(options)}/collection/token/"
+    |> Util.send_post(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def create_oauth_2_token(options \\ []) do
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", Util.collection_auth(options)},
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)}
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/oauth2/token/"
     |> Util.send_post(%{}, headers)
     |> case do
       {:ok, body} -> {:ok, body}
@@ -19,8 +32,6 @@ defmodule ExMtnMomo.Collection do
   end
 
   def request_to_pay(attrs, access_token, uuid4 \\ UUID.uuid4(),  options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
       {"X-Reference-Id", uuid4},
@@ -29,7 +40,7 @@ defmodule ExMtnMomo.Collection do
       {"Content-Type", "application/json"},
     ]
 
-    "#{base_url}/collection/v1_0/requesttopay"
+    "#{Util.extract_base_url(options)}/collection/v1_0/requesttopay"
     |> Util.send_post(attrs, headers)
     |> case do
       {:ok, body} -> {:ok, body}
@@ -38,15 +49,58 @@ defmodule ExMtnMomo.Collection do
   end
 
   def request_to_pay_transaction_status(reference_id, access_token,  options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
       {"X-Target-Environment", Util.extract_x_target_environment(options)},
       {"Authorization", "Bearer #{access_token}"},
     ]
 
-    "#{base_url}/collection/v1_0/requesttopay/#{reference_id}"
+    "#{Util.extract_base_url(options)}/collection/v1_0/requesttopay/#{reference_id}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_account_balance_in_specific_currency(currency, access_token,  options \\ []) do
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/v1_0/account/balance/#{currency}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_account_balance(access_token,  options \\ []) do
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/v1_0/account/balance"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_invoice_status(x_reference_id, access_token,  options \\ []) do
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/v2_0/invoice/#{x_reference_id}"
     |> Util.send_get(%{}, headers)
     |> case do
       {:ok, body} -> {:ok, body}
@@ -55,15 +109,13 @@ defmodule ExMtnMomo.Collection do
   end
 
   def request_to_withdraw_transaction_status(reference_id, access_token,  options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
       {"X-Target-Environment", Util.extract_x_target_environment(options)},
       {"Authorization", "Bearer #{access_token}"},
     ]
 
-    "#{base_url}/collection/v1_0/requesttowithdraw/#{reference_id}"
+    "#{Util.extract_base_url(options)}/collection/v1_0/requesttowithdraw/#{reference_id}"
     |> Util.send_get(%{}, headers)
     |> case do
       {:ok, body} -> {:ok, body}
@@ -72,15 +124,13 @@ defmodule ExMtnMomo.Collection do
   end
 
   def basic_user_info(msisdn, access_token,  options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
       {"X-Target-Environment", Util.extract_x_target_environment(options)},
       {"Authorization", "Bearer #{access_token}"},
     ]
 
-    "#{base_url}/collection/v1_0/accountholder/msisdn/#{msisdn}/basicuserinfo"
+    "#{Util.extract_base_url(options)}/collection/v1_0/accountholder/msisdn/#{msisdn}/basicuserinfo"
     |> Util.send_get(%{}, headers)
     |> case do
       {:ok, body} -> {:ok, body}
@@ -89,15 +139,43 @@ defmodule ExMtnMomo.Collection do
   end
 
   def get_payment_status(x_reference_id, access_token,  options \\ []) do
-    base_url = Util.extract_base_url(options)
-
     headers = [
       {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
       {"X-Target-Environment", Util.extract_x_target_environment(options)},
       {"Authorization", "Bearer #{access_token}"},
     ]
 
-    "#{base_url}/collection/v2_0/payment/#{x_reference_id}"
+    "#{Util.extract_base_url(options)}/collection/v2_0/payment/#{x_reference_id}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_pre_approval_status(reference_id, access_token,  options \\ []) do
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/v2_0/preapproval/#{reference_id}"
+    |> Util.send_get(%{}, headers)
+    |> case do
+      {:ok, body} -> {:ok, body}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def get_user_info_with_consent(access_token,  options \\ []) do
+    headers = [
+      {"Ocp-Apim-Subscription-Key", Util.extract_collection_secondary_key(options)},
+      {"X-Target-Environment", Util.extract_x_target_environment(options)},
+      {"Authorization", "Bearer #{access_token}"},
+    ]
+
+    "#{Util.extract_base_url(options)}/collection/oauth2/v1_0/userinfo"
     |> Util.send_get(%{}, headers)
     |> case do
       {:ok, body} -> {:ok, body}
